e1a8059ebad5c8dc52140e2e822c65a34bef82df
kekuso <kekuso808@gmail.com>
2016-10-14 20:18:30 -1000

    More minor changes to angular

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,23 +1,32 @@
 angular.module('omniMood')
-  .controller('tweetController', [
+  // .controller('tweetController', [
+  //   '$scope',
+  //   'socket',
+  //   function ($scope, socket) {
+  //     $scope.Tweets = [];
+  //     socket.emit('start tweets', true);
+  //     socket.on('tweet', function (tweet) {
+  //       $scope.Tweets.push(tweet);
+  //     });
+  //   }
+  // ])
+  .controller('emojiController', [
     '$scope',
     'socket',
-    function ($scope, socket) {
+    'EmojiFactory',
+    'TweetFactory',
+    function($scope, socket, EmojiFactory, TweetFactory) {
+      $scope.Emojis = [];
       $scope.Tweets = [];
       socket.emit('start tweets', true);
+
       socket.on('tweet', function (tweet) {
         $scope.Tweets.push(tweet);
       });
-    }
-  ])
-  .controller('emojiController', [
-    '$scope',
-    'EmojiFactory',
-    function($scope, EmojiFactory) {
-      $scope.Emojis = [];
+
       EmojiFactory.getEmojis()
         .then(function(emojis) {
-          emojis.data.forEach(function (code) {
+          emojis.data.forEach(function (code, index) {
             $scope.Emojis.push(code);
           });
         });
